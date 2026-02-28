ae7686005fcefb96cd10451b84d96ec763906c0e
Lee Torres <grafrafraftorres28@gmail.com>
2026-02-28 23:47:30 +0800

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,19 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    /**
+     * Resolve the Inertia view prefix based on the current route.
+     * Accounting controllers render 'accounting/...' views by default,
+     * but when accessed via super-accounting routes they render 'super-accounting/...' views.
+     */
+    protected function viewPrefix(): string
+    {
+        $routeName = request()->route()?->getName() ?? '';
+
+        if (str_starts_with($routeName, 'super-accounting.')) {
+            return 'super-accounting';
+        }
+
+        return 'accounting';
+    }
 }
