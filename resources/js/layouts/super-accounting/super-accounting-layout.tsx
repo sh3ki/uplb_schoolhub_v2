f86b1601e7d40caea72e0e038f030756ebffe53f
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-01 04:17:26 +0800

    Update super-accounting-layout.tsx

--- resources/js/layouts/super-accounting/super-accounting-layout.tsx
+++ resources/js/layouts/super-accounting/super-accounting-layout.tsx
@@ -1,12 +1,14 @@
 import { PropsWithChildren } from 'react';
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 import { SuperAccountingSidebar } from '@/components/super-accounting/super-accounting-sidebar';
+import { FlashMessages } from '@/components/flash-messages';
 
 export default function SuperAccountingLayout({ children }: PropsWithChildren) {
     return (
         <SidebarProvider>
             <SuperAccountingSidebar />
             <SidebarInset>{children}</SidebarInset>
+            <FlashMessages />
         </SidebarProvider>
     );
 }
