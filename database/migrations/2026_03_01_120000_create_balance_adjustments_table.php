c462ce5aaf25d93bc9eca44a167be366dc4b3268
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-02 02:46:26 +0800

    Update 2026_03_01_120000_create_balance_adjustments_table.php

--- database/migrations/2026_03_01_120000_create_balance_adjustments_table.php
+++ database/migrations/2026_03_01_120000_create_balance_adjustments_table.php
@@ -8,6 +8,8 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
     public function up(): void
     {
@@ -28,3 +30,12 @@ return new class extends Migration
     }
 
     /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('balance_adjustments');
+    }
+};
