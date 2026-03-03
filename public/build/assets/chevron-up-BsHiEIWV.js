3bf9a6e940d0b28c7afa76fc30d0843eb56238b3
Lee Torres <grafrafraftorres28@gmail.com>
2026-03-04 01:52:29 +0800

    feat: clearance registrar-filter, notes tab, light mode default, class-list leftmost, super-accounting student-accounts, enrollment status fix

--- public/build/assets/chevron-up-BsHiEIWV.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./button-BEN19p1X.js";const e=[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]],n=o("ChevronUp",e);export{n as C};
--- public/build/assets/circle-B9-ToT-G.js
+++
@@ -1,1 +0,0 @@
-import{a as c}from"./button-BEN19p1X.js";const e=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]],r=c("Circle",e);export{r as C};
