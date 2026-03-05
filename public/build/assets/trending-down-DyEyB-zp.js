ae25a08354721d0ef9b29c26c5e724cbf5a3bf65
sh3ki <shekaigarcia@gmail.com>
2026-03-05 14:00:41 +0800

    Delete public/build/assets/trending-down-DyEyB-zp.js

--- public/build/assets/trending-down-DyEyB-zp.js
+++
@@ -1,1 +0,0 @@
-import{a as n}from"./button-Dqt0RYFt.js";const o=[["polyline",{points:"22 17 13.5 8.5 8.5 13.5 2 7",key:"1r2t7k"}],["polyline",{points:"16 17 22 17 22 11",key:"11uiuu"}]],i=n("TrendingDown",o);export{i as T};
