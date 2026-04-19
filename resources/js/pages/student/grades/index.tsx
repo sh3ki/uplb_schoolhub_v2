4270cb46fde9e8bd29ce94d0d0f1f65567d46704
Lee Torres <grafrafraftorres28@gmail.com>
2026-04-20 01:10:13 +0800

    Update index.tsx

--- resources/js/pages/student/grades/index.tsx
+++ resources/js/pages/student/grades/index.tsx
@@ -101,7 +101,7 @@ export default function GradesIndex({
         <StudentLayout breadcrumbs={breadcrumbs}>
             <Head title="My Grades" />
 
-            <div className="space-y-6">
+            <div className="space-y-6 p-6">
                 {/* Header */}
                 <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4">
                     <div>
